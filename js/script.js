8e9dfa1412c8878fc4700f29ef301d49aca727df
Lourine <lourine.millicent@gmail.com>
2020-07-18 07:04:46 +0300

    Add a getAkan function to script.js file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function getAkanName(CC, YY, MM, DD) {
+  return (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+}
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var namesMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var namesFemale = ["Akosua", "Adowa", "Abena", "Akua", "Yaa", "Afua", "Ama"];
+var gender;
+var dayOfTheWeek = getAkanName();
